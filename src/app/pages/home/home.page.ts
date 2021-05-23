57a39f4bb3baa4db7db565bbe5b4c1f3f0977899
Jeison Crespo <jeisoncrespo15@gmail.com>
2021-05-23 18:57:44 -0400

    temas y ng angular agregados

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AnimeService } from '../../services/anime.service';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  skeleton = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  anime: any;
 
-  constructor() {}
-
+  constructor(
+    public animeSvc: AnimeService,
+    public toastController: ToastController
+  ) {}
+  ngOnInit() {
+    this.getEpisodes();
+  }
+  getEpisodes() {
+    setTimeout(() => {
+      this.animeSvc.getLastest().subscribe((res) => {
+        this.anime = res;
+        this.anime = this.anime?.animes;
+      });
+    }, 1000);
+  }
+  async refresh() {
+    if (this.anime) {
+      this.getEpisodes();
+      const toast = await this.toastController.create({
+        header: 'Lista de animes actualizada',
+        animated: true,
+        position: 'top',
+        color: 'primary',
+        mode: 'ios',
+        buttons: [
+          {
+            text: 'Done',
+            role: 'cancel',
+          },
+        ],
+      });
+      toast.present();
+    } else {
+      this.getEpisodes();
+    }
+  }
 }
